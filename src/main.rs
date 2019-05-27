e9ca28f7b9ca7676fc564c88a7f9b678c29a0d35
Taisuke Miyazaki <MiyazakiTaisuke@lifull.com>
2019-05-27 16:10:17 +0900

    ignore line and comment

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,105 @@
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate lazy_static;
 
 use clap::Arg;
 use regex::Regex;
 use std::default::Default;
 use std::error;
+use std::fmt;
 use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 use termcolor::{BufferWriter, Color, ColorChoice, ColorSpec, WriteColor};
 
+struct StatSummary {
+    all: usize,
+    ignored: usize,
+    marked: usize,
+}
+
+impl StatSummary {
+    fn calc(lines: &MarkedFile) -> Self {
+        let mut count = 0;
+        let mut ignore_count = 0;
+        for line in lines.iter() {
+            if line.mark {
+                count += 1;
+            }
+            if line.ignore {
+                ignore_count += 1;
+            }
+        }
+        StatSummary {
+            all: lines.len(),
+            ignored: ignore_count,
+            marked: count,
+        }
+    }
+}
+
+impl fmt::Display for StatSummary {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let ratio = 100u64 * (self.marked as u64) / (self.all as u64);
+        write!(
+            f,
+            "lines: {}, marked: {}, ignored: {}, ratio: {}%\n",
+            self.all,
+            self.marked,
+            self.ignored,
+            ratio,
+        )?;
+        let ratio = 100u64 * (self.marked as u64) / (self.all - self.ignored) as u64;
+        write!(f, "lines: {}, marked: {}, ratio: {}%",
+               self.all - self.ignored, self.marked, ratio)
+    }
+}
+
 enum MarkStat {
     Number(usize),
     Range(usize, usize),
+    IgnoreNumber(usize),
+    IgnoreRange(usize, usize),
     RegexpRange(Regex, Regex),
 }
 
 impl MarkStat {
-    fn parse_line(line: &String) -> Result<Self, Box<error::Error>> {
-        let nums = line.split_whitespace().collect::<Vec<_>>();
-
-        if nums.len() == 1 {
-            let num = nums[0].parse()?;
-            return Ok(MarkStat::Number(num));
-        } else if nums.len() == 2 {
-            let from = nums[0].parse()?;
-            let to = nums[1].parse()?;
-            return Ok(MarkStat::Range(from, to));
+    fn parse_line(line: &String) -> Result<Option<Self>, Box<error::Error>> {
+        lazy_static! {
+            static ref comment: Regex = Regex::new(r"^(#.*)$").unwrap();
+            static ref num: Regex = Regex::new(r"^(\d+)$").unwrap();
+            static ref range: Regex = Regex::new(r"^(\d+)\s(\d+)$").unwrap();
+            static ref ignore_num: Regex = Regex::new(r"^ignore:\s*(\d+)$").unwrap();
+            static ref ignore_range: Regex = Regex::new(r"^ignore:\s*(\d+)\s(\d+)$").unwrap();
         }
+
+        if let Some(_cap) = comment.captures(line) {
+            return Ok(None);
+        }
+
+        if let Some(cap) = num.captures(line) {
+            let num_str = &cap[1];
+            return Ok(Some(MarkStat::Number(num_str.parse()?)));
+        }
+
+        if let Some(cap) = range.captures(line) {
+            let from_str = &cap[1];
+            let to_str = &cap[2];
+            return Ok(Some(MarkStat::Range(from_str.parse()?, to_str.parse()?)));
+        }
+
+        if let Some(cap) = ignore_num.captures(line) {
+            let num_str = &cap[1];
+            return Ok(Some(MarkStat::IgnoreNumber(num_str.parse()?)));
+        }
+
+        if let Some(cap) = ignore_range.captures(line) {
+            let from_str = &cap[1];
+            let to_str = &cap[2];
+            return Ok(Some(MarkStat::IgnoreRange(from_str.parse()?, to_str.parse()?)));
+        }
+
         Err(From::from("spec invalid"))
     }
 }
@@ -36,6 +108,7 @@ impl MarkStat {
 struct Line {
     str: String,
     mark: bool,
+    ignore: bool,
 }
 
 #[derive(Default)]
@@ -63,11 +136,29 @@ impl MarkedFile {
         })
     }
 
+    fn len(&self) -> usize {
+        self.lines.len()
+    }
+
     fn mark(&mut self, line_number: usize) {
         if line_number >= self.lines.len() {
             return;
         }
         self.lines[line_number].mark = true;
+    }
+
+    fn unmark(&mut self, line_number: usize) {
+        if line_number >= self.lines.len() {
+            return;
+        }
+        self.lines[line_number].mark = false;
+    }
+
+    fn ignore(&mut self, line_number: usize) {
+        if line_number >= self.lines.len() {
+            return;
+        }
+        self.lines[line_number].ignore = true;
     }
 
     fn iter(&self) -> MarkedFileIterator {
@@ -106,13 +197,18 @@ fn parse_mark<P: AsRef<Path>>(mark_spec_path: P) -> Result<Vec<MarkStat>, Box<er
         if line.is_empty() {
             continue;
         }
-        mark_spec.push(MarkStat::parse_line(&line)?);
+        let spec = MarkStat::parse_line(&line)?;
+        match spec {
+            Some(spec) => mark_spec.push(spec),
+            None => continue,
+        }
     }
     Ok(mark_spec)
 }
 
 fn main() -> Result<(), Box<error::Error>> {
     let app = app_from_crate!()
+        .arg(Arg::from_usage("[stat] --stat 'print stat'"))
         .arg(Arg::from_usage("-s --source <OPT> 'target source file'"))
         .arg(Arg::from_usage("-c --spec <OPT> 'specification file"));
 
@@ -124,6 +220,8 @@ fn main() -> Result<(), Box<error::Error>> {
     let mut lines = MarkedFile::from_file(source_path)?;
     let mark_spec = parse_mark(mark_spec_path)?;
 
+    let mut ignores = Vec::new();
+
     for mark in mark_spec {
         match mark {
             MarkStat::Number(num) => {
@@ -134,16 +232,34 @@ fn main() -> Result<(), Box<error::Error>> {
                     lines.mark(l - 1);
                 }
             }
+            MarkStat::IgnoreNumber(num) => {
+                ignores.push(num - 1);
+            }
+            MarkStat::IgnoreRange(from, to) => {
+                for l in from..=to {
+                    ignores.push(l - 1);
+                }
+            }
             MarkStat::RegexpRange(from, to) => {
                 unimplemented!("regex");
             }
         }
+    }
+    for ignore_line in ignores.iter() {
+        lines.unmark(*ignore_line);
+        lines.ignore(*ignore_line);
+    }
+
+    if matches.is_present("stat") {
+        println!("{}", StatSummary::calc(&lines));
+        return Ok(());
     }
 
     let bufwriter = BufferWriter::stdout(ColorChoice::Always);
     let mut buffer = bufwriter.buffer();
     for (i, line) in lines.iter().enumerate() {
         let mark = line.mark;
+        let ignored = line.ignore;
         let line = &line.str;
         if mark {
             buffer.set_color(ColorSpec::new().set_fg(Some(Color::Cyan)))?;
@@ -151,6 +267,14 @@ fn main() -> Result<(), Box<error::Error>> {
             buffer.reset()?;
             write!(&mut buffer, "|")?;
             buffer.set_color(ColorSpec::new().set_fg(Some(Color::Green)))?;
+            writeln!(&mut buffer, "{}", line)?;
+            buffer.reset()?;
+        } else if ignored {
+            buffer.set_color(ColorSpec::new().set_fg(Some(Color::Cyan)))?;
+            write!(&mut buffer, "{:>4}", i + 1)?;
+            buffer.reset()?;
+            write!(&mut buffer, "|")?;
+            buffer.set_color(ColorSpec::new().set_fg(Some(Color::Rgb(128, 128, 128))))?;
             writeln!(&mut buffer, "{}", line)?;
             buffer.reset()?;
         } else {
